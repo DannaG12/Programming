8213e5afdfed91bb1fa6cff4a48139487be9e441
DannaG12 <dg0340y@gre.ac.uk>
2026-07-08 14:01:03 +0100

    made visual changes to the app, added different colours

--- app.py
+++ app.py
@@ -6,15 +6,20 @@ app = Dash()
 
 df = pd.read_csv('combined_output.csv')
 
+
 app.layout = html.Div([
-    html.H1("Sales Dashboard"),
+
+    html.H1("Pink Morsels Sales Dashboard", style={
+    'textAlign': 'center', 
+    'color': '#fffffe', 
+    'backgroundColor': '#232946',
+    'minHeight': '25vh'}),
     dcc.Graph(id="graph"),
     dcc.Checklist(
     id="checklist",
     options=["north", "south", "east", "west"],
     value=["north", "south", "east", "west"],
-    inline=True
-    ),
+    inline=True),
 ])
 @app.callback(
     Output("graph", "figure"),
@@ -22,7 +27,13 @@ app.layout = html.Div([
 def update_line_chart(regions):
     filtered_df = df[df['region'].isin(regions)]
     fig = px.line(filtered_df,
-        x="date", y="sales", color='region', title='Sales by Region')
+        x="date", y="sales", color='region',
+        color_discrete_sequence = ['#b8c1ec', '#eebbc3', '#566B81', '#8bd3dd'])
+    fig.update_layout(
+        plot_bgcolor='#232946',   
+        paper_bgcolor='#232946',  
+        font_color='#fffffe'      
+    )
     return fig
 
 
